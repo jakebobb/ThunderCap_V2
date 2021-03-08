f70663b91512ce6bd84d338f89de306c9116dd73
Jake <jjbobb98@gmail.com>
2021-03-08 16:55:31 -0600

    Changed main activity java to most updated format.

--- app/src/main/java/com/bluetoothexample/thundercap_v1/MainActivity.java
+++ app/src/main/java/com/bluetoothexample/thundercap_v1/MainActivity.java
@@ -1,9 +1,10 @@
 package com.bluetoothexample.thundercap_v1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
@@ -14,90 +15,100 @@ import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.Switch;
-import android.widget.EditText;
-import android.widget.ToggleButton;
 import android.os.Handler;
-import android.widget.CompoundButton;
-import android.widget.TextView;
-import android.widget.ArrayAdapter;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.os.Looper;
+import android.os.Message;
+//import android.support.v4.app.ActivityCompat;
+//import android.support.v4.content.ContextCompat;
 //import android.support.v7.app.AppCompatActivity;
-
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+//import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.bluetooth.BluetoothSocket;
+import android.os.Handler;
+import android.os.SystemClock;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Method;
 import java.util.Set;
 import java.util.UUID;
 
-import java.util.concurrent.TimeUnit;
-
 public class MainActivity extends AppCompatActivity {
 
-        private TextView mBluetoothStatus;
-        private TextView mReadBuffer;
-        private Button mScanBtn;
-        private Button mOffBtn;
-        private Button mListPairedDevicesBtn;
-        private Button mDiscoverBtn;
-        private BluetoothAdapter mBTAdapter;
-        private BluetoothSocket mBTSocket = null;
-        private Set<BluetoothDevice> mPairedDevices;
-        private ArrayAdapter<String> mBTArrayAdapter;
-        private ListView mDevicesListView;
+    private final String TAG = MainActivity.class.getSimpleName();
+
+    private static final UUID BT_MODULE_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); // "random" unique identifier
+
+    // #defines for identifying shared types between calling functions
+    private final static int REQUEST_ENABLE_BT = 1; // used to identify adding bluetooth names
+    public final static int MESSAGE_READ = 2; // used in bluetooth handler to identify message update
+    private final static int CONNECTING_STATUS = 3; // used in bluetooth handler to identify message status
+
+    // GUI Components
+    private TextView mBluetoothStatus;
+    private TextView mReadBuffer;
+    private Button mScanBtn;
+    private Button mOffBtn;
+    private Button mListPairedDevicesBtn;
+    private Button mDiscoverBtn;
+    private ListView mDevicesListView;
+
+    private BluetoothAdapter mBTAdapter;
+    private Set<BluetoothDevice> mPairedDevices;
+    private ArrayAdapter<String> mBTArrayAdapter;
+
+    private Handler mHandler; // Our main handler that will receive callback notifications
+    private ConnectedThread mConnectedThread; // bluetooth background worker thread to send and receive data
+    private BluetoothSocket mBTSocket = null; // bi-directional client-to-client data path
     EditText launchDelay1;
     EditText launchDelay2;
     EditText launchDelay3;
     EditText launchDelay4;
-
-
-        private Handler mHandler; // Our main handler that will receive callback notifications
-        private ConnectedThread mConnectedThread; // bluetooth background worker thread to send and receive data
-       // private BluetoothSocket mBTSocket = null; // bi-directional client-to-client data path
-
-        private final static UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-
-        // #defines for identifying shared types between calling functions
-        private final static int REQUEST_ENABLE_BT = 1; // used to identify adding bluetooth names
-        private final static int MESSAGE_READ = 2; // used in bluetooth handler to identify message update
-        private final static int CONNECTING_STATUS = 3; // used in bluetooth handler to identify message status
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         mBluetoothStatus = (TextView)findViewById(R.id.bluetoothStatus);
+        mReadBuffer = (TextView) findViewById(R.id.read_buffer);
         mScanBtn = (Button)findViewById(R.id.scan);
         mOffBtn = (Button)findViewById(R.id.off);
         mDiscoverBtn = (Button)findViewById(R.id.discover);
-        //Switch switch1 = findViewById(R.id.switch1);
+        //mListPairedDevicesBtn = (Button)findViewById(R.id.paired_btn);
+        //mLED1 = (CheckBox)findViewById(R.id.checkbox_led_1);
+        // private CheckBox mLED1;
         Button launchbutton = findViewById(R.id.button);
-        //launchbutton.setBackgroundColor(Color.GREEN);
-        //EditText launchDelay1;
-        //EditText launchDelay2;
-      // EditText launchDelay3;
-       // EditText launchDelay4;
 
-        mBTArrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+        mBTArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
         mBTAdapter = BluetoothAdapter.getDefaultAdapter(); // get a handle on the bluetooth radio
 
-        mDevicesListView = (ListView)findViewById(R.id.devicesListView);
+        mDevicesListView = (ListView)findViewById(R.id.devices_list_view);
         mDevicesListView.setAdapter(mBTArrayAdapter); // assign model to view
         mDevicesListView.setOnItemClickListener(mDeviceClickListener);
 
-        // GUI Handler (Potentially an issue with "Lint" in imports)
-        mHandler = new Handler(){
-            public void handleMessage(android.os.Message msg){
+        // Ask for location permission if not already allowed
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+
+
+        mHandler = new Handler(Looper.getMainLooper()){
+            @Override
+            public void handleMessage(Message msg){
                 if(msg.what == MESSAGE_READ){
                     String readMessage = null;
                     try {
@@ -110,16 +121,13 @@ public class MainActivity extends AppCompatActivity {
 
                 if(msg.what == CONNECTING_STATUS){
                     if(msg.arg1 == 1)
-                        mBluetoothStatus.setText("Connected to Device: " + (String)(msg.obj));
+                        mBluetoothStatus.setText("Connected to Device: " + msg.obj);
                     else
                         mBluetoothStatus.setText("Connection Failed");
                 }
             }
         };
 
-
-
-        // Button Listeners (Shouldn't need additional code)
         if (mBTArrayAdapter == null) {
             // Device does not support Bluetooth
             mBluetoothStatus.setText("Status: Bluetooth not found");
@@ -127,7 +135,7 @@ public class MainActivity extends AppCompatActivity {
         }
         else {
 /*
-            switch1.setOnClickListener(new View.OnClickListener(){
+            mLED1.setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View v){
                     if(mConnectedThread != null) //First check to make sure thread created
@@ -144,39 +152,37 @@ public class MainActivity extends AppCompatActivity {
             });
 
 
-
             mScanBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    bluetoothOn(v);
+                    bluetoothOn();
                 }
             });
 
             mOffBtn.setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View v){
-                    bluetoothOff(v);
+                    bluetoothOff();
                 }
             });
-
-            /* mListPairedDevicesBtn.setOnClickListener(new View.OnClickListener() {
+/*
+            mListPairedDevicesBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v){
-                    listPairedDevices(v);
+                    listPairedDevices();
                 }
             });*/
 
             mDiscoverBtn.setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View v){
-                    discover(v);
+                    discover();
                 }
             });
         }
-
     }
-    // Bluetooth OFF and ON (Shouldn't need to change)
-    private void bluetoothOn(View view){
+
+    private void bluetoothOn(){
         if (!mBTAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
@@ -269,14 +275,13 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void bluetoothOff(View view){
+    private void bluetoothOff(){
         mBTAdapter.disable(); // turn off
         mBluetoothStatus.setText("Bluetooth disabled");
         Toast.makeText(getApplicationContext(),"Bluetooth turned Off", Toast.LENGTH_SHORT).show();
     }
 
-    // Discover Devices (Shouldn't need to be changed)
-    private void discover(View view){
+    private void discover(){
         // Check if the device is already discovering
         if(mBTAdapter.isDiscovering()){
             mBTAdapter.cancelDiscovery();
@@ -308,10 +313,23 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private void listPairedDevices(){
+        mBTArrayAdapter.clear();
+        mPairedDevices = mBTAdapter.getBondedDevices();
+        if(mBTAdapter.isEnabled()) {
+            // put it's one to the adapter
+            for (BluetoothDevice device : mPairedDevices)
+                mBTArrayAdapter.add(device.getName() + "\n" + device.getAddress());
 
-    // Connecting as a Client (Shouldn't need to be changed)
+            Toast.makeText(getApplicationContext(), "Show Paired Devices", Toast.LENGTH_SHORT).show();
+        }
+        else
+            Toast.makeText(getApplicationContext(), "Bluetooth not on", Toast.LENGTH_SHORT).show();
+    }
+
     private AdapterView.OnItemClickListener mDeviceClickListener = new AdapterView.OnItemClickListener() {
-        public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3) {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
             if(!mBTAdapter.isEnabled()) {
                 Toast.makeText(getBaseContext(), "Bluetooth not on", Toast.LENGTH_SHORT).show();
@@ -320,13 +338,14 @@ public class MainActivity extends AppCompatActivity {
 
             mBluetoothStatus.setText("Connecting...");
             // Get the device MAC address, which is the last 17 chars in the View
-            String info = ((TextView) v).getText().toString();
+            String info = ((TextView) view).getText().toString();
             final String address = info.substring(info.length() - 17);
             final String name = info.substring(0,info.length() - 17);
 
             // Spawn a new thread to avoid blocking the GUI one
             new Thread()
             {
+                @Override
                 public void run() {
                     boolean fail = false;
 
@@ -352,8 +371,8 @@ public class MainActivity extends AppCompatActivity {
                             Toast.makeText(getBaseContext(), "Socket creation failed", Toast.LENGTH_SHORT).show();
                         }
                     }
-                    if(fail == false) {
-                        mConnectedThread = new ConnectedThread(mBTSocket);
+                    if(!fail) {
+                        mConnectedThread = new ConnectedThread(mBTSocket, mHandler);
                         mConnectedThread.start();
 
                         mHandler.obtainMessage(CONNECTING_STATUS, 1, -1, name)
@@ -365,154 +384,17 @@ public class MainActivity extends AppCompatActivity {
     };
 
     private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
-        return  device.createRfcommSocketToServiceRecord(BTMODULEUUID);
-        //creates secure outgoing connection with BT device using UUID
-    }
-/*
-    public void onBtnClick(View view) {
-        EditText launchDelay1 = findViewById(R.id.editTextNumber);
-        EditText launchDelay2 = findViewById(R.id.editTextNumber2);
-        EditText launchDelay3 = findViewById(R.id.editTextNumber3);
-        EditText launchDelay4 = findViewById(R.id.editTextNumber4);
-        // Button b1 = findViewById(R.id.button4);
-        String delay1 = launchDelay1.getText().toString();
-        String delay2 = launchDelay2.getText().toString();
-        String delay3 = launchDelay3.getText().toString();
-        String delay4 = launchDelay4.getText().toString();
-
-        int finalValue1 = Integer.parseInt(delay1);
-        int finalValue2 = Integer.parseInt(delay2);
-        int finalValue3 = Integer.parseInt(delay3);
-        int finalValue4 = Integer.parseInt(delay4);
-
-            ToggleButton toggle = (ToggleButton) findViewById(R.id.toggleButton2);
-            toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    if (isChecked) {
-                    Button button = findViewById(R.id.button);
-                        button.setOnClickListener(new View.OnClickListener(){
-                            @Override
-                            public void onClick(View v){
-
-                                final Handler handler = new Handler();
-                                handler.postDelayed(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        Button b1 = findViewById(R.id.button4);
-                                        b1.setBackgroundColor(Color.GREEN);
-
-                                    }
-                                }, finalValue1 * 1000);
-
-                                final Handler handler2 = new Handler();
-                                handler2.postDelayed(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        Button b2 = findViewById(R.id.button2);
-                                        b2.setBackgroundColor(Color.GREEN);
-
-                                    }
-                                }, finalValue2 * 1000);
-
-                                final Handler handler3 = new Handler();
-                                handler3.postDelayed(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        Button b3 = findViewById(R.id.button3);
-                                        b3.setBackgroundColor(Color.GREEN);
-
-                                    }
-                                }, finalValue3 * 1000);
-
-                                final Handler handler4 = new Handler();
-                                handler4.postDelayed(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        Button b4 = findViewById(R.id.button5);
-                                        b4.setBackgroundColor(Color.GREEN);
-
-                                    }
-                                }, finalValue4 * 1000);
-                            }
-                        });
-
-                    }else {
-                            // The toggle is disabled
-
-                    }
-                }
-
-
-            });
-
-    }*/
-
-
-
-
-    private class ConnectedThread extends Thread {
-        private final BluetoothSocket mmSocket;
-        private final InputStream mmInStream;
-        private final OutputStream mmOutStream;
-
-        public ConnectedThread(BluetoothSocket socket) {
-            mmSocket = socket;
-            InputStream tmpIn = null;
-            OutputStream tmpOut = null;
-
-            // Get the input and output streams, using temp objects because
-            // member streams are final
-            try {
-                tmpIn = socket.getInputStream();
-                tmpOut = socket.getOutputStream();
-            } catch (IOException e) { }
-
-            mmInStream = tmpIn;
-            mmOutStream = tmpOut;
+        try {
+            final Method m = device.getClass().getMethod("createInsecureRfcommSocketToServiceRecord", UUID.class);
+            return (BluetoothSocket) m.invoke(device, BT_MODULE_UUID);
+        } catch (Exception e) {
+            Log.e(TAG, "Could not create Insecure RFComm Connection",e);
         }
-
-        // This is not needed as this is reading the input bluetooth stream
-        /*public void run() {
-            byte[] buffer = new byte[1024];  // buffer store for the stream
-            int bytes; // bytes returned from read()
-            // Keep listening to the InputStream until an exception occurs
-            while (true) {
-                try {
-                    // Read from the InputStream
-                    bytes = mmInStream.available();
-                    if(bytes != 0) {
-                        SystemClock.sleep(100); //pause and wait for rest of data. Adjust this depending on your sending speed.
-                        bytes = mmInStream.available(); // how many bytes are ready to be read?
-                        bytes = mmInStream.read(buffer, 0, bytes); // record how many bytes we actually read
-                        mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer)
-                                .sendToTarget(); // Send the obtained bytes to the UI activity
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-
-                    break;
-                }
-            }
-        }*/
-
-        // Call this from the main activity to send data to the remote device
-        // Throw the functionality code here?
-        public void write(String input) {
-            byte[] bytes = input.getBytes();           //converts entered String into bytes
-            try {
-                mmOutStream.write(bytes);
-            } catch (IOException e) { }
-        }
-
-        /* Call this from the main activity to shutdown the connection */
-        public void cancel() {
-            try {
-                mmSocket.close();
-            } catch (IOException e) { }
-        }
+        return  device.createRfcommSocketToServiceRecord(BT_MODULE_UUID);
     }
+}
 
-    }
+
 
 
 
